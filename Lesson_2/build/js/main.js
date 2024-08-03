8e5f08022f5885bd487e4f11e49c7c9924a36e55
swethaas <swetha2001as@gmail.com>
2024-08-03 22:51:09 +0530

    rocket project is finished

--- Lesson_2/build/js/main.js
+++ Lesson_2/build/js/main.js
@@ -1,7 +1,6 @@
 const initApp=()=>{
     const hamburgerBtn = document.getElementById('hamburger-button')
-    const mobileMenu= document.getElementById(mobile-menu) 
-
+    const mobileMenu= document.getElementById('mobile-menu') 
     const toggleMenu = () => {
         mobileMenu.classList.toggle('hidden')
         mobileMenu.classList.toggle('flex')
@@ -10,16 +9,5 @@ const initApp=()=>{
    hamburgerBtn.addEventListener('click',toggleMenu)
    mobileMenu.addEventListener('click',toggleMenu)
 }
-
 document.addEventListener('DOMContentLoaded',initApp)
-
-
-
-
-
-
-
-
-
-
 
